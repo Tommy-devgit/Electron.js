bccd44b12e2813afe71f03444a66413e28970377
Tomas Melesse <tomasmelesse413@gmail.com>
2026-02-24 15:39:28 +0300

    Electron-builder and preload script changes

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,7 +3,12 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  window.electron.getStaticData();
+
 
   return (
     <>
